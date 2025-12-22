af9de5c520a9773c9d7ef677833516a68bfe592f
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-22 20:33:32 +0500

    hmmm IDK what to commit

--- encryption/fernet.py
+++ encryption/fernet.py
@@ -6,9 +6,9 @@ your_32_byte_key = os.urandom(32)
 key =  base64.urlsafe_b64encode(your_32_byte_key)
 f = Fernet(key)
 print(key)
-# token = f.encrypt(b"A really secret message. Not for prying eyes.")
-#
-# print(token)
-# print(f.decrypt(token))
-# print(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+
+print(token)
+print(f.decrypt(token))
+print(key)
 
