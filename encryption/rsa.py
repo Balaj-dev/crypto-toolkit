ed06bcb0ef2b3d7415bd041d2574948d18385a64
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-26 23:41:43 +0500

    commetns

--- encryption/rsa.py
+++ encryption/rsa.py
@@ -1,7 +1,7 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048)
-
-print(private_key)
+# private_key = rsa.generate_private_key(
+#     public_exponent=65537,
+#     key_size=2048)
+#
+# print(private_key)
